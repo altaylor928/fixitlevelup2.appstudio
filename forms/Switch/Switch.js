f33615e5d9ce1eea5e41b10435b14d6bf11dd72b
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-02-07 20:46:17 -0600

    Update Switch.js

--- forms/Switch/Switch.js
+++ forms/Switch/Switch.js
@@ -1,17 +1,3 @@
-//gets variables
-let firstName = prompt("Please enter your first name: ")
-let userState = prompt("Please enter your state (abbreviation): ")
-let userTemp = prompt("Please enter the temperature: ")
-
-//converts State to uppercase
-var upperState = userState.toUpperCase()
-
-//convert first letter of name to uppercase
-let nameCapitalized = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-
-//puts messages into an array
-let messageArray = [`${nameCapitalized}, wear a warm coat, hat, scarf, and gloves.`, `${nameCapitalized}, wear a warm coat, but you won't need a hat, scarf or gloves.`, `${nameCapitalized}, wear your warmest coat, a warm hat, a scarf, and warm gloves.`, `${nameCapitalized}, wear a warm coat, hat, and gloves. Maybe a scarf too.`]
-
 //output of messages with switch
 switch(true) {
   case (upperState == "NE" && userTemp < '32') :
